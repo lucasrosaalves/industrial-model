3080574e83e4ce92603f3c703ac76ce4007f8a01
Lucas Alves <lucasrosaalves@gmail.com>
2025-07-11 17:37:13 -0300

    Bump version to 0.1.41 and refactor assertions in engine parameter generation and test cases for improved clarity

--- tests/test_engine_query.py
+++ tests/test_engine_query.py
@@ -1,26 +1,59 @@
-from industrial_model import InstanceId, PaginatedResult, col, select
+from industrial_model import PaginatedResult, col, select
 
 from .hubs import generate_engine
 from .models import CogniteAsset, CogniteAssetType, CogniteDescribable, CogniteEquipment
 
 
-def test_engine_query() -> None:
+def test_engine_query_describable() -> None:
     engine = generate_engine()
 
     select_statements = [
         select(CogniteDescribable).limit(10),
         select(CogniteDescribable).where(CogniteDescribable.name == "test"),
+    ]
+
+    for statement in select_statements:
+        result = engine.query(statement)
+        assert isinstance(result, PaginatedResult)
+
+
+def test_engine_query_asset_type() -> None:
+    engine = generate_engine()
+
+    select_statements = [
         select(CogniteAssetType)
         .limit(10)
         .where(CogniteAssetType.code == "TESTING_123")
         .asc(CogniteAssetType.code),
+    ]
+
+    for statement in select_statements:
+        result = engine.query(statement)
+        assert isinstance(result, PaginatedResult)
+
+
+def test_engine_query_equipment() -> None:
+    engine = generate_engine()
+
+    select_statements = [
         select(CogniteEquipment).where(col(CogniteEquipment.asset).exists_()).limit(10),
+    ]
+
+    for statement in select_statements:
+        result = engine.query(statement)
+        assert isinstance(result, PaginatedResult)
+
+
+def test_engine_query_asset() -> None:
+    engine = generate_engine()
+
+    select_statements = [
         select(CogniteAsset).where(
             col(CogniteAsset.parent).nested_(CogniteAsset.external_id == "PARENT-123")
         ),
         select(CogniteAsset).where(
             col(CogniteAsset.path).contains_any_(
-                [InstanceId(external_id="CHILD-456", space="cdf_cdm")]
+                [{"externalId": "CHILD-456", "space": "cdf_cdm"}]
             )
         ),
     ]
--- tests/test_engine_search.py
+++ tests/test_engine_search.py
@@ -1,23 +1,53 @@
-from industrial_model import InstanceId, col, search
+from industrial_model import col, search
 
 from .hubs import generate_engine
 from .models import CogniteAsset, CogniteAssetType, CogniteDescribable, CogniteEquipment
 
 
-def test_engine_search() -> None:
+def test_engine_search_describable() -> None:
     engine = generate_engine()
 
     search_statements = [
         search(CogniteDescribable).limit(10),
         search(CogniteDescribable).where(CogniteDescribable.name == "test"),
-        search(CogniteAssetType)
-        .limit(10)
-        .where(CogniteAssetType.code == "TESTING_123")
-        .asc(CogniteAssetType.code),
+    ]
+
+    for statement in search_statements:
+        result = engine.search(statement)
+        assert isinstance(result, list)
+
+
+def test_engine_search_asset_type() -> None:
+    engine = generate_engine()
+
+    search_statements = [
+        search(CogniteAssetType).limit(10).where(CogniteAssetType.code == "TESTING_123")
+    ]
+
+    for statement in search_statements:
+        result = engine.search(statement)
+        assert isinstance(result, list)
+
+
+def test_engine_search_equipment() -> None:
+    engine = generate_engine()
+
+    search_statements = [
         search(CogniteEquipment).where(col(CogniteEquipment.asset).exists_()).limit(10),
+    ]
+
+    for statement in search_statements:
+        result = engine.search(statement)
+        assert isinstance(result, list)
+
+
+def test_engine_search_asset() -> None:
+    engine = generate_engine()
+
+    search_statements = [
         search(CogniteAsset).where(
             col(CogniteAsset.path).contains_any_(
-                [InstanceId(external_id="CHILD-456", space="cdf_cdm")]
+                [{"externalId": "CHILD-456", "space": "cdf_cdm"}]
             )
         ),
     ]
