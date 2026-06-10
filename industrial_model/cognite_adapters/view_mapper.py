29119ad4777a63f416ce2c7b88fc90162b440cf9
Lucas Alves <lucas.alves@radixeng.com>
2026-06-10 18:25:08 -0300

    feat: enhance ViewMapper to load dependent views dynamically

--- industrial_model/cognite_adapters/view_mapper.py
+++ industrial_model/cognite_adapters/view_mapper.py
@@ -3,7 +3,9 @@ import asyncio
 from cognite.client import AsyncCogniteClient
 from cognite.client.data_classes.data_modeling import (
     View,
+    ViewId,
 )
+from cognite.client.data_classes.data_modeling.views import ViewProperty
 
 from industrial_model.config import DataModelId
 
@@ -36,6 +38,41 @@ class ViewMapper:
                 ids=self._data_model_id.as_tuple(),
                 inline_views=True,
             )
-            self._views_as_dict = {
-                view.external_id: view for view in dm.latest_version().views
-            }
+
+            views = dm.latest_version().views
+
+            while True:
+                new_dependency_view_ids = self._get_new_dependency_view_ids(views)
+                if not new_dependency_view_ids:
+                    break
+
+                new_views = await self._cognite_client.data_modeling.views.retrieve(
+                    ids=new_dependency_view_ids,
+                )
+                views.extend(new_views)
+
+            self._views_as_dict = {view.external_id: view for view in views}
+
+    def _get_new_dependency_view_ids(self, views: list[View]) -> list[ViewId]:
+        view_ids = {view.external_id for view in views}
+        views_from_dependencies = [
+            self._try_extract_view_id(view_property)
+            for view in views
+            for view_property in view.properties.values()
+        ]
+        new_views: set[ViewId] = set()
+        for views_dependency in views_from_dependencies:
+            if (
+                views_dependency
+                and views_dependency.external_id not in view_ids
+                and views_dependency not in new_views
+            ):
+                new_views.add(views_dependency)
+        return list(new_views)
+
+    def _try_extract_view_id(self, view_property: ViewProperty) -> ViewId | None:
+        if hasattr(view_property, "source") and isinstance(
+            view_property.source, ViewId
+        ):
+            return view_property.source
+        return None
